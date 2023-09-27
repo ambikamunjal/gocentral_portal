668d07197e54177369f22c925877181be75b8c3d
ambikamunjal <amunjal@sscinc.com>
2023-09-27 14:59:24 -0400

    Update Table.vue

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -191,7 +191,7 @@ table.bordered-table td {
    width: 45px;
    /*height: 20px;*/
    padding: 3px;
-   background: #FF0000 ; /* #FA9C1B ;*/
+   background:#FA9C1B ; /*#FF0000 ;*/ /* #FA9C1B ;*/
    border-radius: 10px;
  }
 
